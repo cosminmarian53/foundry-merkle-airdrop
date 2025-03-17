6056394a5e121be890b1441ba79eee13ca95ad27
cosminmarian53 <cosmin.lavric@student.unitbv.ro>
2025-03-18 00:34:12 +0200

    New scripts added

---
+++ test/MerkleAirdrop.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+
+contract MerkleAirdropTest is Test {
+    function test() public {
+        // test code here
+    }
+}
