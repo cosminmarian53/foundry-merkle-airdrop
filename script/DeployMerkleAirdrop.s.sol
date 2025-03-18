2fd2a87c00792498f73b2e4160191bfa2a7e171a
cosminmarian53 <cosmin.lavric@student.unitbv.ro>
2025-03-18 23:33:16 +0200

    Test written for MerkleAirdrop

---
+++ script/DeployMerkleAirdrop.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {MerkleAirdrop} from "src/MerkleAirdrop.sol";
+import {BagelToken} from "src/BagelToken.sol";
+import {Script} from "forge-std/Script.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -1,10 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Test} from "forge-std/Test.sol";
-
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "src/MerkleAirdrop.sol";
+import {BagelToken} from "src/BagelToken.sol";
 contract MerkleAirdropTest is Test {
-    function test() public {
-        // test code here
+    MerkleAirdrop public airdrop;
+    BagelToken public token;
+    address user;
+    address gasPayer;
+    bytes32 merkleRoot =
+        0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 amountToCollect = (25 * 1e18); // 25.000000
+    uint256 amountToSend = amountToCollect * 4;
+
+    bytes32 proofOne =
+        0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo =
+        0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] proof = [proofOne, proofTwo];
+    uint256 userPrivKey;
+    function setUp() public {
+        token = new BagelToken();
+        airdrop = new MerkleAirdrop(merkleRoot, token);
+        token.mint(token.owner(), amountToSend);
+        token.transfer(address(airdrop), amountToSend);
+        gasPayer = makeAddr("gasPayer");
+        (user, userPrivKey) = makeAddrAndKey("user");
+    }
+    function testUsersCanClaim() public {
+        uint256 startingBalance = token.balanceOf(user);
+        vm.prank(user);
+        airdrop.claim(user, amountToCollect, proof);
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("Ending balance: %d", endingBalance);
+        assertEq(endingBalance - startingBalance, amountToCollect);
     }
 }
