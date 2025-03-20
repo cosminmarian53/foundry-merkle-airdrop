6f17caac59cb44f5f4b9342192f1eec79e7082dc
cosminmarian53 <cosmin.lavric@student.unitbv.ro>
2025-03-20 19:37:28 +0200

    All scripts have been written

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -27,10 +27,12 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
 contract MerkleAirdrop is EIP712 {
     using SafeERC20 for IERC20;
     // some list of addresses
     // allow someone in the list to claim the airdrop
+
     struct AirdropClaim {
         address account;
         uint256 amount;
@@ -38,12 +40,14 @@ contract MerkleAirdrop is EIP712 {
     /*//////////////////////////////////////////////////////////////
                                  ERRORS
     //////////////////////////////////////////////////////////////*/
+
     error MerkleAirdrop__InvalidProof();
     error MerkleAirdrop__UserAlreadyClaimed();
     error MerkleAirdrop__InvalidSignature();
     /*//////////////////////////////////////////////////////////////
                                  EVENTS
     //////////////////////////////////////////////////////////////*/
+
     event Claim(address indexed account, uint256 amount);
 
     /*//////////////////////////////////////////////////////////////
@@ -52,17 +56,13 @@ contract MerkleAirdrop is EIP712 {
     address[] claimers;
     bytes32 private immutable i_merkleRoot;
     IERC20 private immutable i_airdropToken;
-    bytes32 private constant MESSAGE_TYPEHASH =
-        keccak256("AirdropClaim(address account,uint256 amount)");
+    bytes32 private constant MESSAGE_TYPEHASH = keccak256("AirdropClaim(address account,uint256 amount)");
     mapping(address claimer => bool hasClaimed) private s_claimed;
 
     /*//////////////////////////////////////////////////////////////
                              CONSTRUCTOR
     //////////////////////////////////////////////////////////////*/
-    constructor(
-        bytes32 merkleRoot,
-        IERC20 airdropToken
-    ) EIP712("MerkleAirdrop", "1") {
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) EIP712("MerkleAirdrop", "1") {
         i_merkleRoot = merkleRoot;
         i_airdropToken = airdropToken;
     }
@@ -70,23 +70,16 @@ contract MerkleAirdrop is EIP712 {
     /*//////////////////////////////////////////////////////////////
                                  EXTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
-    function claim(
-        address account,
-        uint256 amount,
-        bytes32[] calldata merkleProof,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external {
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof, uint8 v, bytes32 r, bytes32 s)
+        external
+    {
         if (s_claimed[account]) {
             revert MerkleAirdrop__UserAlreadyClaimed();
         }
         if (!_isValidSignature(account, getMessageHash(account, amount), v, r, s)) {
             revert MerkleAirdrop__InvalidSignature();
         }
-        bytes32 leaf = keccak256(
-            bytes.concat(keccak256(abi.encodePacked(account, amount)))
-        );
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encodePacked(account, amount))));
         if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
             revert MerkleAirdrop__InvalidProof();
         }
@@ -97,31 +90,27 @@ contract MerkleAirdrop is EIP712 {
     //*//////////////////////////////////////////////////////////////
     //                           INTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
-    function _isValidSignature(
-        address account,
-        bytes32 digest,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) internal pure returns (bool) {
-        (address actualSigner, , ) = ECDSA.tryRecover(digest, v, r, s);
+
+    function _isValidSignature(address account, bytes32 digest, uint8 v, bytes32 r, bytes32 s)
+        internal
+        pure
+        returns (bool)
+    {
+        (address actualSigner,,) = ECDSA.tryRecover(digest, v, r, s);
         return actualSigner == account;
     }
     /*//////////////////////////////////////////////////////////////
                                  GETTERS
     //////////////////////////////////////////////////////////////*/
+
     function getMerkleRoot() external view returns (bytes32) {
         return i_merkleRoot;
     }
-    function getMessageHash(
-        address account,
-        uint256 amount
-    ) public view returns (bytes32) {
-        return
-            _hashTypedDataV4(
-                keccak256(abi.encode(MESSAGE_TYPEHASH, account, amount))
-            );
+
+    function getMessageHash(address account, uint256 amount) public view returns (bytes32) {
+        return _hashTypedDataV4(keccak256(abi.encode(MESSAGE_TYPEHASH, account, amount)));
     }
+
     function getAirdropToken() external view returns (IERC20) {
         return i_airdropToken;
     }
--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -15,15 +15,12 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
     address user;
     uint256 userPrivKey;
 
-    bytes32 merkleRoot =
-        0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    bytes32 merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     uint256 amountToCollect = (25 * 1e18); // 25.000000
     uint256 amountToSend = amountToCollect * 4;
 
-    bytes32 proofOne =
-        0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
-    bytes32 proofTwo =
-        0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] proof = [proofOne, proofTwo];
 
     function setUp() public {
@@ -40,14 +37,8 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
         (user, userPrivKey) = makeAddrAndKey("user");
     }
 
-    function signMessage(
-        uint256 privKey,
-        address account
-    ) public view returns (uint8 v, bytes32 r, bytes32 s) {
-        bytes32 hashedMessage = airdrop.getMessageHash(
-            account,
-            amountToCollect
-        );
+    function signMessage(uint256 privKey, address account) public view returns (uint8 v, bytes32 r, bytes32 s) {
+        bytes32 hashedMessage = airdrop.getMessageHash(account, amountToCollect);
         (v, r, s) = vm.sign(privKey, hashedMessage);
     }
 
